b05f426c38159d226d0489e4b94044d0c5977427
{自分の名前} <misaki.ishihara0004@gmail.com>
2022-07-03 14:27:05 +0900

    added hooks (login function with useMutation)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,32 @@ const queryClient = new QueryClient({
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('/dashboard')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+  supabase.auth.onAuthStateChange((event, _) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/dashboard')
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/')
+    }
+  })
+  useEffect(() => {
+    validateSession()
+  },[])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps}/>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
